1068b1a134e4db01284f9f15dd9477b63626c41c
Malte Peters <mp@nect.com>
2018-04-09 19:10:07 +0200

    chore: revert

--- library/src/main/java/com/google/android/cameraview/CameraView.java
+++ library/src/main/java/com/google/android/cameraview/CameraView.java
@@ -292,7 +292,7 @@ public class CameraView extends FrameLayout {
             //store the state and restore this state after fall back to Camera1
             Parcelable state=onSaveInstanceState();
             // Camera2 uses legacy hardware layer; fall back to Camera1
-            //mImpl = new Camera1(mCallbacks, createPreviewImpl(getContext()));
+            mImpl = new Camera1(mCallbacks, createPreviewImpl(getContext()));
             onRestoreInstanceState(state);
             mImpl.start();
         }
